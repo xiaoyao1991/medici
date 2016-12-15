7f977786f00d5f89a7a349271945f45820e894cc
Tarrant <tarrantzhang@gmail.com>
2016-12-15 02:00:05 -0600

    Deposit tested

--- adsEx/contracts/AdExchange.sol
+++ adsEx/contracts/AdExchange.sol
@@ -2,6 +2,38 @@ pragma solidity ^0.4.2;
 
 contract AdExchange {
   uint public tov = 144; // term of validity; assuming 10min a block, that's 24hours
+  event debugging(address sender, string message, uint index);
+
+  //========================================================= debugging function
+  function bytes32ToString (bytes32 data) returns (string) {
+      bytes memory bytesString = new bytes(32);
+      for (uint j=0; j<32; j++) {
+          byte char = byte(bytes32(uint(data) * 2 ** (8 * j)));
+          if (char != 0) {
+              bytesString[j] = char;
+          }
+      }
+      return string(bytesString);
+  }
+
+  function getAllAdvertise(){
+    for(uint i = 0; i < advertiserList.length;i++){
+      debugging(advertiserList[i].publicKey,"hah", i);
+    }
+  }
+  function getAllPublisher(){
+    for(uint i = 0; i < publisherList.length;i++){
+      debugging(publisherList[i],"hah",i);
+    }
+  }
+  function getDepositInfo(address advertiser){
+    DepositEntry[] entryList = depositTable[advertiser];
+
+    for(uint i = 0; i<entryList.length; i++) {
+      debugging(entryList[i].recipient,"deposit info",entryList[i].amount);
+    }
+  }
+
 
   // ======================================================== advertiser
   struct Advertiser {
@@ -60,22 +92,32 @@ contract AdExchange {
       callback: callback
     }));
     advertiserCallbacks[msg.sender] = callback;
+    debugging(msg.sender,callback,0);
   }
 
   function deposit()
-    /*payable*/
+    payable
     isRegisteredAdvertiser
   {
     DepositEntry[] entryList = depositTable[msg.sender];
 
+    if(entryList.length == 0) throw;
+
+    uint totalDepositAmount = 0;
     for(uint i = 0; i<entryList.length; i++) {
+      uint amountEach = msg.value/entryList.length * publisherWeighting[i];
       entryList[i] = DepositEntry({
         recipient: entryList[i].recipient,
-        amount: entryList[i].amount + msg.value * publisherWeighting[i] / 1000
+        amount: entryList[i].amount + amountEach
       });
+      totalDepositAmount += amountEach;
     }
 
     depositTable[msg.sender] = entryList;
+    //return the change individable
+    if (!msg.sender.send(msg.value-totalDepositAmount)) {
+      throw;
+    }
   }
 
   function findAvailableAdvertisersByPublisher(address publisher) constant returns (address[]) {
@@ -104,7 +146,10 @@ contract AdExchange {
   uint[] public publisherWeighting;
 
   modifier isNewPublisher(){
-    if(publisherDedup[msg.sender]) throw;
+    if(publisherDedup[msg.sender]) {
+        //debugging(msg.sender, "deuplicate publisher");
+        throw;
+    }
     _;
   }
 
@@ -116,6 +161,7 @@ contract AdExchange {
   function registerPublisher()
     isNewPublisher
   {
+
     // create new channels for new publisher to advertisers
     for(uint i = 0; i<advertiserList.length; i++){
       depositTable[advertiserList[i].publicKey].push(DepositEntry({
@@ -208,22 +254,4 @@ contract AdExchange {
     //TODO -- should called by advertiser to adjust deposit balancing
   }
 
-  /*function cleanWithdrawTable(){
-    for(uint i = 0; i<withdrawHistoryTable.length; i++){
-      WithdrawHistoryEntry row = withdrawHistoryTable[i];
-      if(row.blockHeightAtBid + tov <  block.number){
-        delete
-      }
-    }
-  }*/
-
-  /*function arrangeDeposit(address advertiser, uint deposit, uint[] weighting) internal{
-    DepositEntry[] entryList = depositTable[advertiser];
-    for(uint i = 0; i<entryList.length;i++){
-      entryList[i] = DepositEntry({
-        recipient: entryList[i].recipient,
-        amount: entryList[i].amount + deposit * weighting[i]/1000
-      });
-    }
-  }*/
 }
--- adsEx/testAd.js
+++ adsEx/testAd.js
@@ -1,15 +1,46 @@
+var crypto = require('./bidding_server/lib/crypto');
+var utils = require('ethereumjs-util');
+
 module.exports = function(callback) {
 
   function initContract(contract){
     var publisher1 = "0x90f8bf6a479f320ead074411a4b0e7944ea8c9c1";
+    var publisher2 = "0xe11ba2b4d45eaed5996cd0823791e0c93114882d";
+    var publisher3 = "0x22d491bde2303f2f43325b2108d26f1eaba1e32b";
     var advertiser1 = "0xffcf8fdee72ac11b5c542428b35eef5769c409f0";
-    contract.registerPublisher(publisher1);
+    var advertiser1sk = "6cbed15c793ce57650b9877cf6fa156fbef513c4e6134f022a85b1ffdd59b2a1"
+    var debugging = contract.debugging();
+    debugging.watch(function(err, result) {
+      if (err) {
+       console.log(err);
+       return;
+     }
+     console.log("address: " + result.args.sender + ", message:" + result.args.message + ", index:" + result.args.index);
+     //debugging.stopWatching()
+    });
 
-    // var balance = web3.eth.getBalance(advertiser1);
-    // console.log(balance);
-    contract.deposit({from: advertiser1, value:100000})
-    var rtv = contract.getDeposit({from:advertiser1})//.then(console.log);
-    console.log(rtv.then(console.log))
+    //register 3 publishers
+    // contract.registerPublisher({from: publisher1});
+    // contract.registerPublisher({from: publisher2});
+    // contract.registerPublisher({from: publisher3});
+    // contract.getAllPublisher();
+
+    //register advertiser
+    var callback = "www.google.com";
+    var vrs = utils.fromRpcSig(crypto.sign(advertiser1sk, callback));
+    // contract.getAllAdvertise();
+  //  contract.registerAdvertiser(callback,vrs.v, utils.bufferToHex(vrs.r), utils.bufferToHex(vrs.s),{from:advertiser1});
+    var balance = web3.eth.getBalance(advertiser1);
+    console.log("before deposit: "+ balance);
+    contract.deposit({from:advertiser1,value:10000});
+    balance = web3.eth.getBalance(advertiser1);
+    console.log("after deposit: "+ balance);
+    contract.getDepositInfo(advertiser1);
+
+
+    // contract.deposit({from: advertiser1, value:100000})
+    // var rtv = contract.getDeposit({from:advertiser1})//.then(console.log);
+    // console.log(rtv.then(console.log))
     // contract.debugAdd().then(console.log)
     // contract.debug().then(console.log)
     //contract.depositTable(advertiser1,advertiser1).then(console.log)
