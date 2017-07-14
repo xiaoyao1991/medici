222d6cad3580d57b60709d1a69720a145435e846
Xiaoyao Qian <jivepaulrock@gmail.com>
2017-07-14 00:49:54 -0700

    validate signature + first price model

--- example/advertiser/routes/index.js
+++ example/advertiser/routes/index.js
@@ -2,17 +2,10 @@ var express = require('express');
 var router = express.Router();
 var mediciUtils = require('medici-js');
 
-var contractSrc = process.env.CONTRACT;
-var deployedAt = process.env.DEPLOYEDAT;
-var medici = mediciUtils.init(contractSrc, deployedAt);
-
-var biddingExpectation = {};
-var bidIds = {};
-
 var BID = 1;
 var FOLD = -1;
 
-var ads = {
+var ASSETS = {
   "nike": '<img src="http://modernmediamix.com/wp-content/uploads/2013/04/NIKE.jpg"/>',
   "adidas": '<img src="http://www.adidas.com/dwstatic/aaqx_prd/on/demandware.static/Sites-adidas-US-Site/-/default/dw721d387e/images/favicons/favicon.png"/>'
 }
@@ -24,30 +17,18 @@ var brand = process.env.BRAND;
 router.post('/', function(req, res, next) {
   var publisher = req.body.publisherPk;
   var eventId = req.body.eventId;
-  var currentBid = req.body.currentBid;
-
-  if (!biddingExpectation.hasOwnProperty(publisher + "|" + eventId)) {
-    biddingExpectation[publisher + "|" + eventId] = Math.random() * 10 + 1;
-    bidIds[publisher + "|" + eventId] = 0;
-  }
-  var maxExpectedBid = biddingExpectation[publisher + "|" + eventId];
-
-  if (maxExpectedBid <= currentBid) {
-    return res.json({"resp": FOLD});
-  }
 
   // sign a micropayment signature and send
-  var bidId = bidIds[publisher + "|" + eventId]++;
-  var amt = currentBid + 1;
-  var ad = ads[brand];
+  var amt = Math.random() * 10 + 1;
+  var ad = ASSETS[brand];
   var currentBlockId = mediciUtils.getCurrentBlock();
-  var sig = mediciUtils.sign(sk, [publisher, eventId, currentBlockId, bidId, ad, amt]);
+  var sig = mediciUtils.sign(sk, [publisher, eventId, currentBlockId, ad, amt]);
 
   var resp = {
     "resp": BID,
+    "sender": pk,
     "receiver": publisher,
     "eventId": eventId,
-    "bidId": bidId,
     "amt": amt,
     "ad": ad,
     "currentBlockId": currentBlockId,
